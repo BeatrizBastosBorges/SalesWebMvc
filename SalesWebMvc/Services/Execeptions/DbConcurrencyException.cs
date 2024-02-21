5e887fddf77ff92587834be23dc6e5cfedf8b401
37772470819 <37772470819@SEPLAN-0411>
2024-02-21 15:22:50 -0300

    Adicionar arquivos de projeto.

---
+++ SalesWebMvc/Services/Execeptions/DbConcurrencyException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SalesWebMvc.Services.Execeptions
+{
+    public class DbConcurrencyException : ApplicationException
+    {
+        public DbConcurrencyException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ SalesWebMvc/Services/Execeptions/NotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SalesWebMvc.Services.Execeptions
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException (string message) : base(message)
+        {
+        }
+    }
+}
